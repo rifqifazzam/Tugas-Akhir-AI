aa373a2c7d5199568b7f8c6f150390a2a66b0343
rifqifazzam <rifqifauzan709@gmail.com>
2023-12-09 14:45:31 +0700

    Menambahkan penjelsan kode

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Deklarasi variabel global untuk papan permainan, simbol pemain manusia, simbol pemain bot, dan kombinasi menang
 let papanPermainan;
 let pManusia ='O';
 let pBot = 'X';
@@ -12,53 +13,81 @@ const comboMenang =[
   [0, 3, 6]
 ];
 
+// Mendapatkan semua elemen dengan kelas 'cell' pada HTML
 const cells = document.querySelectorAll('.cell');
+
+// Memulai permainan
 startGame();
 
-function selectSimbol(simbol){
+// Fungsi untuk memilih simbol pemain
+function pilihSimbol(simbol){
   pManusia = simbol;
   pBot = simbol==='O' ? 'X' :'O';
   papanPermainan = Array.from(Array(9).keys());
+
+  // Menambahkan event listener untuk setiap sel pada papan permainan
   for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', turnClick, false);
   }
+
+  // Jika simbol bot adalah 'X', maka bot melakukan langkah pertamanya
   if (pBot === 'X') {
-    turn(bestSpot(),pBot);
+    turn(bestSpot(), pBot);
   }
-  document.querySelector('.selectSimbol').style.display = "none";
+
+  // Menyembunyikan tampilan pemilihan simbol setelah pemain memilih simbol
+  document.querySelector('.pilihSimbol').style.display = "none";
 }
 
+// Fungsi untuk memulai permainan
 function startGame() {
   document.querySelector('.endgame').style.display = "none";
   document.querySelector('.endgame .text').innerText ="";
-  document.querySelector('.selectSimbol').style.display = "block";
+  document.querySelector('.pilihSimbol').style.display = "block";
+
+  // Mengatur ulang tampilan sel pada papan permainan
   for (let i = 0; i < cells.length; i++) {
     cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
   }
 }
 
+// Fungsi yang dipanggil ketika pemain mengklik suatu sel pada papan permainan
 function turnClick(square) {
-  if (typeof papanPermainan[square.target.id] ==='number') {
+  if (typeof papanPermainan[square.target.id] === 'number') {
+    // Pemain manusia melakukan langkah
     turn(square.target.id, pManusia);
-    if (!checkWin(papanPermainan, pManusia) && !checkTie())  
-    setTimeout(function () {
-      turn(bestSpot(), pBot);
-    }, 1000);
+
+    // Jika belum ada pemenang atau seri, bot melakukan langkahnya s
+    if (!cekMenang(papanPermainan, pManusia) && !cekSeri()) {
+      setTimeout(function () {
+        turn(bestSpot(), pBot);
+      }, 700); // Delay waktu sebanyak 1000 milidetik (1 detik), dapat disesuaikan
+    }
   }
 }
 
+// Fungsi untuk mengeksekusi langkah pemain pada suatu sel
 function turn(squareId, player) {
   papanPermainan[squareId] = player;
   document.getElementById(squareId).innerHTML = player;
-  let gameWon = checkWin(papanPermainan, player);
-  if (gameWon) gameOver(gameWon);
-  checkTie();
+  let gameWon = cekMenang(papanPermainan, player);
+
+  // Jika terdapat pemenang, panggil fungsi gameOver
+  if (gameWon) {
+    gameOver(gameWon);
+  }
+
+  // Memeriksa apakah permainan berakhir dengan seri
+  cekSeri();
 }
 
-function checkWin(board, player) {
+// Fungsi untuk mengecek apakah terdapat pemenang
+function cekMenang(board, player) {
   let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
   let gameWon = null;
+
+  // Iterasi melalui kombinasi menang untuk mengecek apakah pemain telah menang
   for (let [index, win] of comboMenang.entries()) {
     if (win.every(elem => plays.indexOf(elem) > -1)) {
       gameWon = {index: index, player: player};
@@ -68,71 +97,95 @@ function checkWin(board, player) {
   return gameWon;
 }
 
+// Fungsi yang dipanggil ketika terdapat pemenang
 function gameOver(gameWon){
+  // Menandai sel-sel yang membentuk kombinasi menang dengan warna
   for (let index of comboMenang[gameWon.index]) {
     document.getElementById(index).style.backgroundColor = 
       gameWon.player === pManusia ? "blue" : "red";
   }
+
+  // Menonaktifkan event listener pada sel-sel untuk menghindari klik lebih lanjut
   for (let i=0; i < cells.length; i++) {
     cells[i].removeEventListener('click', turnClick, false);
   }
+
+  // Menampilkan pesan hasil permainan
   declareWinner(gameWon.player === pManusia ? "Kamu Menang!" : "Kamu kalah");
 }
 
+// Fungsi untuk menampilkan pesan hasil permainan
 function declareWinner(who) {
   document.querySelector(".endgame").style.display = "block";
   document.querySelector(".endgame .text").innerText = who;
 }
-function emptySquares() {
+
+// Fungsi untuk mendapatkan sel-sel yang masih kosong pada papan permainan
+function cekSpotKosong() {
   return papanPermainan.filter((elm, i) => i===elm);
 }
   
+// Fungsi untuk mendapatkan langkah terbaik yang dapat diambil oleh bot
 function bestSpot(){
   return minimax(papanPermainan, pBot).index;
 }
   
-function checkTie() {
-  if (emptySquares().length === 0){
+// Fungsi untuk mengecek apakah permainan berakhir dengan seri
+function cekSeri() {
+  if (cekSpotKosong().length === 0){
+    // Jika permainan seri, mewarnai sel-sel dengan warna hijau
     for (cell of cells) {
       cell.style.backgroundColor = "green";
       cell.removeEventListener('click',turnClick, false);
     }
+
+    // Menampilkan pesan hasil permainan
     declareWinner("Game seri!");
     return true;
   } 
   return false;
 }
 
+// Fungsi untuk mengimplementasikan algoritma minimax untuk pemilihan langkah terbaik oleh bot
 function minimax(papanBaru, player) {
-  var availSpots = emptySquares(papanBaru);
+  var availSpots = cekSpotKosong(papanBaru);
   
-  if (checkWin(papanBaru, pManusia)) {
+  // Jika pemain manusia menang, berikan skor -10
+  if (cekMenang(papanBaru, pManusia)) {
     return {score: -10};
-  } else if (checkWin(papanBaru, pBot)) {
+  } 
+  // Jika pemain bot menang, berikan skor 10
+  else if (cekMenang(papanBaru, pBot)) {
     return {score: 10};
-  } else if (availSpots.length === 0) {
+  } 
+  // Jika tidak ada spot kosong, berikan skor 0 (seri)
+  else if (availSpots.length === 0) {
     return {score: 0};
   }
   
+  // Inisialisasi array untuk menyimpan langkah-langkah yang mungkin
   var moves = [];
+  
+  // Iterasi melalui seluruh spot kosong dan rekursif mencari skor untuk setiap langkah
   for (let i = 0; i < availSpots.length; i ++) {
     var move = {};
     move.index = papanBaru[availSpots[i]];
     papanBaru[availSpots[i]] = player;
     
+    // Panggil rekursif minimax untuk mendapatkan skor langkah
     if (player === pBot)
       move.score = minimax(papanBaru, pManusia).score;
     else
        move.score =  minimax(papanBaru, pBot).score;
 
+    // Kembalikan papan permainan ke keadaan sebelumnya
     papanBaru[availSpots[i]] = move.index;
-    
-    if ((player === pBot && move.score === 10) || (player === pManusia && move.score === -10))
-      return move;
-    else 
-      moves.push(move);
+
+    // Tambahkan langkah ke array moves
+    moves.push(move);
   }
   
+  // Pilih langkah terbaik berdasarkan skor
   let bestMove, bestScore;
   if (player === pBot) {
     bestScore = -1000;
@@ -152,5 +205,6 @@ function minimax(papanBaru, player) {
     }
   }
   
+  // Kembalikan langkah terbaik
   return moves[bestMove];
 }
